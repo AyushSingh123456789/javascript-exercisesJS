7e2b125d1228bb81c304e4eefd9692efa8294d1d
Ayush Singh <ayushpratap251@gmail.com>
2026-07-09 18:05:35 +0000

    another one

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (n1) {
+  let convertedTemp1 = parseFloat(((n1 - 32) / 1.8).toFixed(1));
+  return convertedTemp1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (n2) {
+  let convertedTemp2 = parseFloat(((1.8 * n2) + 32).toFixed(1));
+  return convertedTemp2;
 };
 
 // Do not edit below this line
